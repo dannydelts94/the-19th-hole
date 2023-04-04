4083512c037fa873bf9fc93ab22a50600df65d22
Mickey Camarco <48422179+mcamarco@users.noreply.github.com>
2023-04-04 13:27:31 -0400

    added golf course API text search

--- script/script.js
+++ script/script.js
@@ -1,42 +1,31 @@
 
-
-// need to set lat/lon based on zip code?
-// let map;
-
-// async function initMap() {
-//   //@ts-ignore
-//   const { Map } = await google.maps.importLibrary("maps");
-
-//   map = new Map(document.getElementById("map"), {
-//     center: { lat: -34.397, lng: 150.644 },
-//     zoom: 8,
-//   });
-// }
-
-// initMap();
 var map;
 var service;
 var infowindow;
 
+
+// todo: setup latlng using input from user
 function initMap() {
- var location = new google.maps.LatLng(-39.9526,75.1652);
+ var location = new google.maps.LatLng(39.9526, -75.1652);
 
 map = new google.maps.Map(document.getElementById('map'), {
     center: location,
-    zoom: 15
+    zoom: 8
   });
 
 var request = {
   location: location,
   radius: '50',
-  type: ['liquor_store']
+  query: "golf course"
 };
 
   var service = new google.maps.places.PlacesService(map);
 
-  service.nearbySearch(request, function(results, status) {
+  service.textSearch(request, function(results, status) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
         console.log(results)
+
+// todo: set markers based on search results
     //   for (var i = 0; i < results.length; i++) {
     //     createMarker(results[i]);
     //   }
@@ -44,5 +33,3 @@ var request = {
     }
   });
 }
-
-// service.nearbySearch(request, callback);
