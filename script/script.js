3d3621d394d2f081b482a440615a375536fad822
Mickey Camarco <48422179+mcamarco@users.noreply.github.com>
2023-04-05 13:44:59 -0400

    added openweather API to switch from zipcode to LAtLON

--- script/script.js
+++ script/script.js
@@ -1,11 +1,45 @@
 var map;
 var service;
 var infowindow;
+// var APIKeyGoogle = "AIzaSyCfEbgTYj5gyLq7womR6gc4xBQxDa3moZE";
+var APIKeyWeather = "383165bf31a4af053f333f929519989c";
 
 
-// todo: setup latlng using input from user
-function initMap() {
-    var location = new google.maps.LatLng(39.9526, -75.1652);
+// TODO: Event listener for submit button
+$("#formSubmit").on("submit", function (event) {
+    event.preventDefault();
+
+    var zipCode = $("#zip").val();
+    getCoord(zipCode);
+    console.log(zipCode);
+})
+
+
+
+// TODO: Get lat/lon from zipcode
+function getCoord(zipCode) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&limit=5&appid=${APIKeyWeather}`)
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            var lat = data.coord.lat;
+            var lon = data.coord.lon;
+            console.log(lat, lon);
+            // currentWeather(lat, lon);
+            // forecastWeather(lat,lon);
+
+            initMap(lat, lon);
+
+        })
+}
+
+
+
+//  map information
+function initMap(lat, lon) {
+    var location = new google.maps.LatLng(lat, lon);
 
     map = new google.maps.Map(document.getElementById('map'), {
         center: location,
@@ -14,6 +48,7 @@ function initMap() {
 
     var request = {
         location: location,
+        // input field for radius
         radius: '50',
         query: "golf course"
     };
@@ -26,15 +61,27 @@ function initMap() {
 
     var service = new google.maps.places.PlacesService(map);
 
+    // Array for Golf Courses
     service.textSearch(request, function (results, status) {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
             console.log(results)
+
+            // TODO: ADD FOR LOOP
+            for (request i = 0; i < 5; i++) {
+                results = results * i;
+            }
+
         }
     });
 
+    // Array for breweries
     service.textSearch(request2, function (results, status) {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
             console.log(results)
+            // TODO: ADD FOR LOOP - max 5
         }
+
     })
 }
+
+
